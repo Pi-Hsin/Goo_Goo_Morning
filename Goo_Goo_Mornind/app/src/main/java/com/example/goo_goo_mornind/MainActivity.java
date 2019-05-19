981cf2fefe08767aa7fcf9138983f2d2392b5060
ChingChingYa <33059957+ChingChingYa@users.noreply.github.com>
2019-05-19 22:28:55 +0800

    MainActivity Version 1_Completed

--- Goo_Goo_Mornind/app/src/main/java/com/example/goo_goo_mornind/MainActivity.java
+++ Goo_Goo_Mornind/app/src/main/java/com/example/goo_goo_mornind/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.goo_goo_mornind;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private Button button;
+    private MyAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //預設有3個鬧鐘，之後再看怎們儲存值
+        ArrayList<String> myDataset = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            myDataset.add(Integer.toString(i));
+
+        }
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        button=(Button)findViewById(R.id.button);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    //新增鬧鐘
+    public void addAlerm(View view){
+
     }
 }
---
+++ Goo_Goo_Mornind/app/src/main/java/com/example/goo_goo_mornind/MyAdapter.java
@@ -0,0 +1,65 @@
+package com.example.goo_goo_mornind;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<String> mData;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ImageButton imageButton;
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.textView);
+            imageButton = (ImageButton) v.findViewById(R.id.imageButton);
+            // 點擊項目時
+            mTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(),
+                            "click " +getAdapterPosition(),Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // 點擊項目中的Button時
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // 按下Button要做的事，可以開關鬧鐘
+                }
+            });
+        }
+    }
+
+    public MyAdapter(List<String> data) {
+        mData = data;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_alerm, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        //顯示鬧鐘時間
+        holder.mTextView.setText(mData.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+}
